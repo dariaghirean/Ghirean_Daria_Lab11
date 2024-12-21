e0791f1de04cb16025c47a252363b3ee30b13abe
DARIA-ȘTEFANA GHIREAN <daria.ghirean@stud.ubbcluj.ro>
2024-12-21 21:47:43 +0200

    Add project files.

---
+++ Ghirean_Daria_Lab11/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Ghirean_Daria_Lab11
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
---
+++ WebAPI/Models/ShopList.cs
@@ -0,0 +1,8 @@
+﻿namespace WebAPI.Models
+{
+    public class ShopList
+    {
+        public int ID{ get; set; }
+        public string Description { get; set; }
+        public DateTime Date { get; set; } }
+}
